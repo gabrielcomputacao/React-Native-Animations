0e359b76eb1495c5941e7837804a5b6bc84efc43
gabriel <gabrielcomputacao@outlook.com>
2023-12-29 07:01:20 -0300

    feat: doing shake animation

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,12 @@
-import { View } from 'react-native';
+import { View } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect } from "react";
 
 interface Props {
   total: number;
@@ -9,10 +15,25 @@ interface Props {
 
 export function ProgressBar({ total, current }: Props) {
   const percentage = Math.round((current / total) * 100);
+  const sharedProgress = useSharedValue(percentage);
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress}%`,
+    };
+  });
+
+  /* o sharedProgress nao muda com variaveis que nao sao observaveis , por isso precisa fazer um useEffect para observar a variavel e 
+  notificar o sharedvaled quando mudar, porque o sharedvalue é como se fosse o useState so que em relação as animações que dependem desse
+  valor
+*/
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage);
+  }, [current]);
 
   return (
     <View style={styles.track}>
-      <View style={[styles.progress, { width: `${percentage}%` }]} />
+      <Animated.View style={[styles.progress, styleAnimated]} />
     </View>
   );
 }
