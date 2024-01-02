1dff275a42c20f8c70323cb7bb71cceeaa9d2a7c
gabriel <gabrielcomputacao@outlook.com>
2024-01-02 06:10:32 -0300

    feat: creating congratulations page with trophy and stars

---
+++ src/components/Starts/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-end",
+    alignContent: "center",
+  },
+  canvas: {
+    width: 257,
+    height: 249,
+    zIndex: 1,
+    position: "absolute",
+  },
+});
